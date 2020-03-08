6ecc0af7a795d5d8fb0335e2a6bd9dae0ca7e2e4
Garrett Luu <garrettluu131@gmail.com>
2020-03-07 18:16:29 -0800

    Added feature to disable downloading twice

--- js/background.js
+++ js/background.js
@@ -3,11 +3,44 @@ chrome.downloads.onCreated.addListener((downloadedItem) => {
     console.log("downloaded file");
     console.log(downloadedItem);
 
-    chrome.storage.local.get(['carbon'], (result) => {
-        var carbon = result.carbon ? result.carbon : 0;
-        carbon += downloadedItem.fileSize;
-        chrome.storage.local.set({carbon: carbon}, () => {
-            console.log("we did it");
+    chrome.storage.local.get(['lastdownloaded'], (link) => {
+        console.log(link.lastdownloaded);
+        console.log(downloadedItem.url);
+        if (!link) {
+
+            console.log("link is null?")
+            chrome.storage.local.get(['carbon'], (result) => {
+                var carbon = result.carbon ? result.carbon : 0;
+                carbon += downloadedItem.fileSize;
+                chrome.storage.local.set({ carbon: carbon }, () => {
+                    console.log("we did it");
+                });
+            });
+        } else {
+
+
+            console.log("yay");
+            if (link.lastdownloaded === downloadedItem.url) {
+                console.log("cancelled download");
+                chrome.downloads.cancel(downloadedItem.id);
+                if (downloadedItem.state == "complete") {
+                    chrome.downloads.removeFile(item.id);
+                }
+            } else {
+                chrome.storage.local.get(['carbon'], (result) => {
+                    var carbon = result.carbon ? result.carbon : 0;
+                    carbon += downloadedItem.fileSize;
+                    chrome.storage.local.set({ carbon: carbon }, () => {
+                        console.log("we did it");
+                    });
+                });
+            }
+
+
+        }
+        chrome.storage.local.set({ lastdownloaded: downloadedItem.url }, () => {
+            console.log("last url set");
         });
     });
+
 });
