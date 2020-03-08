3087495934d15c4d9ee352fd43a82fb4b7e0af9b
Daniel Truong <ddtruong06@gmail.com>
2020-03-07 16:33:48 -0800

    Linked popup with the carbon emission value in storage

--- js/animate-numbers.js
+++
@@ -1,9 +0,0 @@
-$({ Counter: 0 }).animate({
-  Counter: $('.Count').text()
-}, {
-  duration: 1000,
-  easing: 'swing',
-  step: function() {
-    $('.Count').text(Math.ceil(this.Counter));
-  }
-});
--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,15 @@
-chrome.storage.local.get(['carbon'], (result) => {
+chrome.storage.sync.get(['carbon'], (result) => {
   var countElement = document.getElementsByClassName("Count")[0];
-
   countElement.textContent = result.carbon;
 
+  // Animation
+  $({ Counter: 0 }).animate({
+    Counter: $('.Count').text()
+  }, {
+    duration: 1000,
+    easing: 'swing',
+    step: function() {
+      $('.Count').text(Math.ceil(this.Counter));
+    }
+  });
 });
