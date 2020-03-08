936dfb0884133a99bdb1101b5152b8a1e631a552
Garrett Luu <garrettluu131@gmail.com>
2020-03-07 16:38:48 -0800

    added carbon tracking for downloads

--- js/background.js
+++ js/background.js
@@ -1,2 +1,13 @@
-chrome.tabs.onActivated.addListener(function(activeInfo) {
+chrome.downloads.onCreated.addListener((downloadedItem) => {
+    //get data of downloaded file
+    console.log("downloaded file");
+    console.log(downloadedItem);
+
+    chrome.storage.local.get(['carbon'], (result) => {
+        var carbon = result.carbon ? result.carbon : 0;
+        carbon += downloadedItem.fileSize;
+        chrome.storage.local.set({carbon: carbon}, () => {
+            console.log("we did it");
+        });
+    });
 });
